458b71f97653678086c397fe453e396b60501dba
annineedscoffee <char.anne.spencer@gmail.com>
2017-12-01 19:30:32 -0700

    initial commit

---
+++ study-redux/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title" style={{fontSize: '50px'}}>Welcome to the Redux Study Guide</h1>
+        </header>
+        <p><strong>How this guide works:</strong> cd into study-redux once you download this guide. Start up the webpage and compare it to the code used to create it as prompted.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
